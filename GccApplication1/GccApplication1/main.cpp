d75b4612de90c9760a4634a498695211f078a522
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-03-31 17:24:14 -0300

    Updated.

---
+++ GccApplication1/GccApplication1/main.cpp
@@ -0,0 +1,63 @@
+#include <avr/io.h> // Registros del microcontrolador
+#define F_CPU 16000000UL // Defino la frecuencia de oscilador en 8MHz
+#include <util/delay.h> // Retardos por software/*
+
+
+int main(void)
+{
+  DDRC = ~(1<<PORTC0) & ~(1<<PORTC1); // Configura como entrada
+  DDRD = 0xFF; // Configura como salida
+
+  unsigned char one[] = {
+    0b01010101, 
+    0b10101010 
+  }; 
+  
+  
+  unsigned char two[] = {
+	  0b00000001,
+	  0b00000010,
+	  0b00000100,
+	  0b00001000,
+	  0b00010000,
+	  0b00100000,
+	  0b01000000,
+	  0b10000000
+  };
+
+  unsigned char three[] = {
+	  0b10000000,
+	  0b00000001,
+	  0b01000000,
+	  0b00000010,
+	  0b00100000,
+	  0b00000100,
+	  0b00010000,
+	  0b00001000
+  };
+
+
+  unsigned char act = 0, i = 0; 
+  while(1){
+    if (PINC & (1<<PINC0)) {
+		act++; 
+		act %= 3; 
+		i = 0; 
+		_delay_ms(200); 
+	}
+    if (PINC & (1<<PINC1)) {
+      PORTB |= (1<<3); 
+      PORTB |= (1<<3); 
+    } else {
+      PORTB &= ~(1<<3); 
+      PORTB &= ~(1<<3); 
+    } 
+
+    if (act == 0) PORTD = one[i], ++i, i %= 2; 
+    else if (act == 1) PORTD = two[i], ++i, i %= 8; 
+    else if (act == 2) PORTD = three[i], ++i, i %= 8; 
+    _delay_ms(100); 
+  }
+
+  return 0;
+}
