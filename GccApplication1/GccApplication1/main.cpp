865ea6e6d64f4aee91aa0538a0a556d8174d2454
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-03-31 22:04:28 -0300

    Updated code.

--- GccApplication1/GccApplication1/main.cpp
+++ GccApplication1/GccApplication1/main.cpp
@@ -2,40 +2,37 @@
 #define F_CPU 16000000UL // Defino la frecuencia de oscilador en 8MHz
 #include <util/delay.h> // Retardos por software/*
 
+unsigned char one[] = {
+	0b01010101,
+	0b10101010
+};
+  
+unsigned char two[] = {
+	0b00000001,
+	0b00000010,
+	0b00000100,
+	0b00001000,
+	0b00010000,
+	0b00100000,
+	0b01000000,
+	0b10000000
+};
+
+unsigned char three[] = {
+	0b10000000,
+	0b00000001,
+	0b01000000,
+	0b00000010,
+	0b00100000,
+	0b00000100,
+	0b00010000,
+	0b00001000
+};
 
 int main(void)
 {
   DDRC = ~(1<<PORTC0) & ~(1<<PORTC1); // Configura como entrada
   DDRD = 0xFF; // Configura como salida
-
-  unsigned char one[] = {
-    0b01010101, 
-    0b10101010 
-  }; 
-  
-  
-  unsigned char two[] = {
-	  0b00000001,
-	  0b00000010,
-	  0b00000100,
-	  0b00001000,
-	  0b00010000,
-	  0b00100000,
-	  0b01000000,
-	  0b10000000
-  };
-
-  unsigned char three[] = {
-	  0b10000000,
-	  0b00000001,
-	  0b01000000,
-	  0b00000010,
-	  0b00100000,
-	  0b00000100,
-	  0b00010000,
-	  0b00001000
-  };
-
 
   unsigned char act = 0, i = 0; 
   while(1){
@@ -43,11 +40,11 @@ int main(void)
 		act++; 
 		act %= 3; 
 		i = 0; 
-		_delay_ms(200); 
+		_delay_ms(100); 
 	}
     if (PINC & (1<<PINC1)) {
-      PORTB |= (1<<3); 
-      PORTB |= (1<<3); 
+      if ((act + 1) & 1) PORTB |= (1<<3); 
+      if ((act + 1) & 2) PORTB |= (1<<4); 
     } else {
       PORTB &= ~(1<<3); 
       PORTB &= ~(1<<3); 
