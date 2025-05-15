0d2888814a4c3b02f84ae1c02e06fe6946448334
Bautista <bautax1234@gmail.com>
2025-05-15 11:12:41 -0300

    Fixed game timer

--- entregable2/software/Juego.c
+++ entregable2/software/Juego.c
@@ -1,9 +1,11 @@
 #include "Juego.h"
 
+#define PERIODO_MEF 100
+
 /* Variables para funcionamiento de MEF */
 extern volatile uint8_t MEF_flag=0;
 extern volatile uint8_t cont_MEF=0; 
-uint8_t volatile State_call_count = 0;
+uint16_t volatile State_call_count = 0;
 
 typedef enum {INIT, SHOW_PASSWORD, INPUT_PASSWORD, VERIFY_PASSWORD, WIN, LOSE} eSystem_state;
 static eSystem_state System_state;
@@ -23,8 +25,8 @@ void JUEGO_Init(void) {
 void JUEGO_Update(void) {
     State_call_count++;  // Numero de interrupciones totales  
     uint8_t key;         // Lecturas de teclado
-    uint8_t start;       // Inicio del juego + tiempo transcurrido (para mostrar en pantalla)
-    uint8_t delta;
+    static uint16_t start;       // Inicio del juego + tiempo transcurrido (para mostrar en pantalla)
+    uint16_t delta;
     static uint32_t count = 0;           // Contador para generar delay 
     static uint8_t init_flag = 0;        // Flag para mostrar mensaje de inicio (una sola vez)
     static uint8_t * password;           // Palabra random a adivinar
@@ -52,7 +54,7 @@ void JUEGO_Update(void) {
                 LCDGotoXY(0, 0);
                 LCDstring(password, LONGITUD_PALABRA);
             }
-            else if(count >= 10) {
+            else if(count >= 20) {
                 start = State_call_count;
                 LCDclr(); 
                 LCDGotoXY(0, 1);
@@ -88,10 +90,7 @@ void JUEGO_Update(void) {
             else incorrect++;
             caracter_ingresado = 0;  
 
-            if(correct == LONGITUD_PALABRA) { 
-                System_state = WIN;
-                delta = (State_call_count - start) * 100; 
-            }
+            if(correct == 1) {System_state = WIN;}
             else if(incorrect == ERROR_MAX) System_state = LOSE;
             else System_state = INPUT_PASSWORD;
             break;
@@ -103,9 +102,10 @@ void JUEGO_Update(void) {
                 LCDGotoXY(0, 1);
                 LCDstring("Tiempo: ", 8);  // Mostrar tiempo transcurrido
                 char time_str[6];
-                sprintf(time_str, "%d", delta);
+                sprintf(time_str, "%u", (uint16_t)((State_call_count - start)*PERIODO_MEF));
                 LCDstring(time_str, strlen(time_str));
                 LCDstring("ms", 2);
+                _delay_ms(5000);
             }
             else if(count == 50) {  // Delay de 5000ms (50Ts)
                 LCDclr();
